dd63fe518a727070662c554840de367fbddb8c5a
Kiriakov Mihail <kiriakovmihail@gmail.com>
2018-05-13 19:56:35 +0300

    Create new abstract class Crypto that now implement Currency, update 3 classes that extend it

--- src/Task1/Dogecoin.php
+++ src/Task1/Dogecoin.php
@@ -3,30 +3,8 @@
 namespace Cryptocurrency\Task1;
 
 
-class Dogecoin implements Currency
+class Dogecoin extends Crypto
 {
-    private $name = 'Dogecoin';
-    private $dailyPrice;
-    private $logoUrl = 'https://s2.coinmarketcap.com/static/img/coins/32x32/74.png';
-
-    public function __construct($dailyPrice)
-    {
-        $this->dailyPrice = $dailyPrice;
-    }
-
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function getDailyPrice(): float
-    {
-        return $this->dailyPrice;
-    }
-
-    public function getLogoUrl(): string
-    {
-        return $this->logoUrl;
-    }
+    protected $name = 'Dogecoin';
+    protected $logoUrl = 'https://s2.coinmarketcap.com/static/img/coins/32x32/74.png';
 }
--- src/Task1/Ethereum.php
+++ src/Task1/Ethereum.php
@@ -3,30 +3,8 @@
 namespace Cryptocurrency\Task1;
 
 
-class Ethereum implements Currency
+class Ethereum extends Crypto
 {
-    private $name = 'Ethereum';
-    private $dailyPrice;
-    private $logoUrl = 'https://s2.coinmarketcap.com/static/img/coins/32x32/1027.png';
-
-    public function __construct($dailyPrice)
-    {
-        $this->dailyPrice = $dailyPrice;
-    }
-
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function getDailyPrice(): float
-    {
-        return $this->dailyPrice;
-    }
-
-    public function getLogoUrl(): string
-    {
-        return $this->logoUrl;
-    }
+    protected $name = 'Ethereum';
+    protected $logoUrl = 'https://s2.coinmarketcap.com/static/img/coins/32x32/1027.png';
 }
